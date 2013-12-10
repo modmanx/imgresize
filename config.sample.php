0a6875814ddc0175e83a43adea5e38b557330bda
simon mares <simon.mares@gmail.com>
2013-12-10 13:18:24 -0200

    update sample config

--- config.sample.php
+++ config.sample.php
@@ -1,1 +1,8 @@
-config
+<?php
+
+return array(
+	'rackspace_api_username' => 'api_username',
+	'rackspace_api_key' => 'long_api_key_____',
+	'rackspace_container' => 'create_container_and_enter_here',
+	'cache_dir' => 'cache' . DIRECTORY_SEPARATOR // make sure that this exists
+);
